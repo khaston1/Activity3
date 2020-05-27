edea27150546f5487d1caf5e7513e6e74478b963
khaston1 <steelers3368@yahoo.com>
2020-05-26 21:45:51 -0500

    added code

---
+++ Activity3/Activity3.py
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+
+    <title>Grade Calculator </title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+</head>
+<body>
+
+
+<br>
+
+<h1>Grade Calculator</h1>
+
+<br>
+  <div id="wrapper">
+      <section>
+
+            <p> Welcome to the Grade Calculator </p>
+
+
+
+      </section>
+  </div>
+
+</body>
+</html>
